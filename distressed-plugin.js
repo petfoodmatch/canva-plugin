90b98f25341b949b7bd589025b070a14fb8c0306
petfoodmatch <bigfunpartyshop@gmail.com>
2024-10-18 17:23:08 -0700

    Create distressed-plugin.js

---
+++ distressed-plugin.js
@@ -0,0 +1,7 @@
+{
+  "name": "Distressed Filter Plugin",
+  "description": "Applies a distressed filter to designs for a vintage look.",
+  "version": "1.0",
+  "entry": "https://petfoodmatch.github.io/canva-plugin/distressed-plugin.js",
+  "permissions": ["canva:edit"]
+}
